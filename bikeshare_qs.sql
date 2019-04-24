253c1ea990976451ea1ac86ea5b1b657f0e40abd
daddyprasad5 <daddyprasad5@gmail.com>
2019-04-24 10:46:44 -0500

    updating bikeshare sql script

--- bikeshare_qs.sql
+++ bikeshare_qs.sql
@@ -14,3 +14,20 @@ from weather
 where
     events = 'Rain' and
     zip = 94301;
+
+select zip, weather.date
+from weather
+where maxtemperaturef = (select max(maxtemperaturef) from weather)
+
+select start_station,
+    count(*) as "number of trips started"
+from trips group by start_station
+
+select trip_id,
+    duration
+from trips
+where duration = (select min(duration) from trips);
+
+select AVG(duration), end_station
+from trips
+group by end_station;
